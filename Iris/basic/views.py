7c34b33b7eeaaed1f22f7ec2629258da68b4372e
Satwik Goyal <satwikgoyal@Satwiks-MacBook-Air.local>
2023-09-24 14:51:58 +0100

    basic understanding complete

--- Iris/basic/views.py
+++ Iris/basic/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def welcome(request):
-    return  render(request , 'index.html')
 
+from joblib import load
+model = load('./models/model.joblib')
+
+def predictor(request):
+    if request.method == 'POST':
+        sepal_length = request.POST['sepal_length']
+        sepal_width = request.POST['sepal_width']
+        petal_length = request.POST['petal_length']
+        petal_width = request.POST['petal_width']
+        y_pred = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
+        if y_pred[0] == 0:
+            y_pred = 'Setosa'
+        elif y_pred[0] == 1:
+            y_pred = 'Verscicolor'
+        else:
+            y_pred = 'Virginica'
+        return render(request, 'index.html', {'result' : y_pred})
+    return render(request, 'index.html')
 # Create your views here.
